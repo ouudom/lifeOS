761917ae09fdd0340cd06ce8c09c83babe8d342a
ouudom <vuthyouthdom.01@gmail.com>
2025-11-21 15:08:17 +0700

    fix

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,13 @@
 from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "LifeOS Backend"
     
     # Database
-    DATABASE_URL: str 
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
     
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
 
